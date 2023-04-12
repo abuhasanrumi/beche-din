432b738bddedee75fe2a9eeb9dcc9339fc5bb0c3
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2023-04-13 01:55:43 +0600

    working on hero section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "./Header.css";
 
 const Header = () => {
   return (
     <section className="h-wrapper">
-      <div className="h-container">
-        <img src="./logo.png" alt="" />
-        <div className="h-menu">
+      <div className="h-container paddings innerWidth flexCenter">
+        <img className="logo" src="./logo2.svg" alt="" />
+        <div className="flexCenter h-menu">
           <a href="#">Residencies</a>
           <a href="#">Our Values</a>
           <a href="#">Contact Us</a>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import "./Hero.css";
+import { HiLocationMarker } from "react-icons/hi";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="hero-wrapper">
+      <div className="paddings innerWidth flexCenter hero-container">
+        {/* left side  */}
+        <div className="flexColStart hero-left">
+          <div className="hero-title">
+            <div className="orange-circle" />
+            <h1>
+              Discover <br /> Most Suitable <br />
+              Property
+            </h1>
+          </div>
+          <div className="flexColStart hero-des">
+            <span>Find a variety of properties that suit you very easilty</span>
+            <span>Forget all difficulties in finding a residence for you</span>
+          </div>
+
+          <div className="search-bar">
+            <HiLocationMarker />
+          </div>
+        </div>
+        {/* right side */}
+        <div className="flexCenter hero-right">
+          <div className="image-container">
+            <img src="./hero-image.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
