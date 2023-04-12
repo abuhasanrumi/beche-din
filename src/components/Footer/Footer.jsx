bc45c00e503bb488cb16da6ea5ae9ecd5e44e2c8
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2023-04-13 05:49:49 +0600

    footer section complete

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Footer.css";
+
+export const Footer = () => {
+  return (
+    <section className="f-wrapper">
+      <div className="paddings innerWidth flexCenter f-container">
+        {/* left side */}
+        <div className="flexColStart f-left">
+          <img src="./logo3.svg" width={"160px"} alt="" />
+          <span className="secondaryText">
+            Our vision is to make all people
+            <br /> the best place to live for them.
+          </span>
+        </div>
+
+        {/* right side */}
+        <div className="flexColStart f-right">
+          <span className="primaryText">Information</span>
+          <span className="secondaryText">
+            Gulshan, Mogbazar Flyover, Dhaka Bangladesh
+          </span>
+
+          <div className="flexCenter f-menu">
+            <span>Property</span>
+            <span>Services</span>
+            <span>Product</span>
+            <span>About Us</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
